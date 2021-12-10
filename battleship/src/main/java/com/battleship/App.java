4914fb0fb90341cc9f4dfe2f08f962c27fb77aa3
tommivk <tommikokk@gmail.com>
2021-12-10 20:04:12 +0200

    initialize database when app starts

--- battleship/src/main/java/com/battleship/App.java
+++ battleship/src/main/java/com/battleship/App.java
@@ -1,10 +1,23 @@
 package com.battleship;
 
+import java.sql.*;
 import java.sql.SQLException;
 import com.battleship.ui.BattleshipUi;
 
 public class App {
     public static void main(String[] args) throws SQLException {
+        Connection db = DriverManager.getConnection("jdbc:sqlite:data.db");
+        try {
+            Statement s = db.createStatement();
+            String createUsers = "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE);";
+            String createGame = "CREATE TABLE IF NOT EXISTS Games(id INTEGER PRIMARY KEY, finished BOOLEAN,playeroneshots INTEGER, playertwoshots INTEGER, winner INTEGER, playerone INTEGER, playertwo INTEGER, FOREIGN KEY(playerone) REFERENCES Users(id),FOREIGN KEY(playertwo) REFERENCES Users(id), FOREIGN KEY(winner) REFERENCES Users(id));";
+            s.execute(createUsers);
+            s.execute(createGame);
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            db.close();
+        }
         BattleshipUi.main(args);
     }
 }
