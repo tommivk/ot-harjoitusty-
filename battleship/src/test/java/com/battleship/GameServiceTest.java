3d330801e80d5ee773ff16e79b21879dd5a2636f
tommivk <tommikokk@gmail.com>
2021-12-11 17:31:36 +0200

    fix tests

--- battleship/src/test/java/com/battleship/GameServiceTest.java
+++ battleship/src/test/java/com/battleship/GameServiceTest.java
@@ -28,7 +28,7 @@ public class GameServiceTest {
             s.execute(dropUsers);
             s.execute(dropGame);
             String createUsers = "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE);";
-            String createGame = "CREATE TABLE IF NOT EXISTS Games(id INTEGER PRIMARY KEY, finished BOOLEAN,playeroneshots INTEGER, playertwoshots INTEGER, winner INTEGER, playerone INTEGER, playertwo INTEGER, FOREIGN KEY(playerone) REFERENCES Users(id),FOREIGN KEY(playertwo) REFERENCES Users(id), FOREIGN KEY(winner) REFERENCES Users(id));";
+            String createGame = "CREATE TABLE IF NOT EXISTS Games(id INTEGER PRIMARY KEY, finished BOOLEAN,playeroneshots INTEGER, playertwoshots INTEGER, playeronehits INTEGER, playertwohits INTEGER, winner INTEGER, playerone INTEGER, playertwo INTEGER, FOREIGN KEY(playerone) REFERENCES Users(id),FOREIGN KEY(playertwo) REFERENCES Users(id), FOREIGN KEY(winner) REFERENCES Users(id));";
 
             s.execute(createUsers);
             s.execute(createGame);
