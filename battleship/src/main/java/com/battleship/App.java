190656d592e600ec4a533bd5eee9061cdf1189e8
tommivk <tommikokk@gmail.com>
2021-12-11 14:44:47 +0200

    add computer user in database when app starts

--- battleship/src/main/java/com/battleship/App.java
+++ battleship/src/main/java/com/battleship/App.java
@@ -11,8 +11,10 @@ public class App {
             Statement s = db.createStatement();
             String createUsers = "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE);";
             String createGame = "CREATE TABLE IF NOT EXISTS Games(id INTEGER PRIMARY KEY, finished BOOLEAN,playeroneshots INTEGER, playertwoshots INTEGER, winner INTEGER, playerone INTEGER, playertwo INTEGER, FOREIGN KEY(playerone) REFERENCES Users(id),FOREIGN KEY(playertwo) REFERENCES Users(id), FOREIGN KEY(winner) REFERENCES Users(id));";
+            String addComputer = "INSERT OR IGNORE INTO Users(username) VALUES(\"Computer\");";
             s.execute(createUsers);
             s.execute(createGame);
+            s.execute(addComputer);
         } catch (SQLException e) {
             System.out.println(e);
         } finally {
